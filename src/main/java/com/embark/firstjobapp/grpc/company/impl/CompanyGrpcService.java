140093150dd4fec15827e094f6229d8246c88fa7
Kranti Sambhav <krantisambhav7@gmail.com>
2026-02-04 12:58:26 +0530

    New changes

--- src/main/java/com/embark/firstjobapp/grpc/company/impl/CompanyGrpcService.java
+++ src/main/java/com/embark/firstjobapp/grpc/company/impl/CompanyGrpcService.java
@@ -1,13 +1,10 @@
 package com.embark.firstjobapp.grpc.company.impl;
-
 import java.io.ObjectInputFilter.Status;
-
 import com.embark.firstjobapp.company.Company;
 import com.embark.firstjobapp.company.CompanyService;
 import com.embark.firstjobapp.grpc.company.CompanyServiceGrpc;
 import com.embark.firstjobapp.grpc.company.CompanyIdRequest;
 import com.embark.firstjobapp.grpc.company.CompanyResponse;
-
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -24,16 +21,14 @@ public class CompanyGrpcService extends CompanyServiceGrpc.CompanyServiceImplBas
     public void getCompanyById(CompanyIdRequest request,
                                StreamObserver<CompanyResponse> responseObserver) {
 
-                                Company company = companyService.findById(
+                        Company company = companyService.findById(
                                     Long.valueOf(request.getId())
                             );
                             if (company == null) {
                                 return;
                             }
                             
-
-
-                            CompanyResponse response = CompanyResponse.newBuilder()
+                        CompanyResponse response = CompanyResponse.newBuilder()
                             .setId(company.getId().intValue())
                             .setName(company.getName())
                             .setDescription(company.getDescription())
